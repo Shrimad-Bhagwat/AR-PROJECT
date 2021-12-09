f48f7d8fbe0ca66738b14cc4ab62fd47574847d6
Shrimad Bhagwat <sybbhagwat010102@gmail.com>
2021-12-09 10:19:53 +0530

    changes

--- Assets/Scripts/UIContentFitter.cs
+++ Assets/Scripts/UIContentFitter.cs
@@ -11,10 +11,13 @@ public class UIContentFitter : MonoBehaviour
         HorizontalLayoutGroup hg = GetComponent<HorizontalLayoutGroup>();
         int childCount = transform.childCount - 1;
         float childWidth = transform.GetChild(0).GetComponent<RectTransform>().rect.width;
+        float width = hg.spacing * childCount +
+                      childCount *childWidth+
+                      hg.padding.left +
+                      childWidth;
 
-        float width = hg.spacing * childCount + childWidth * childCount + hg.padding.left ;
-
-        GetComponent<RectTransform>().sizeDelta = new Vector2(width,240);
+        Vector2 size = GetComponent<RectTransform>().sizeDelta;
+        GetComponent<RectTransform>().sizeDelta = new Vector2(width,size.y);
 
     }
 
@@ -24,3 +27,5 @@ public class UIContentFitter : MonoBehaviour
         
     }
 }
+
+
